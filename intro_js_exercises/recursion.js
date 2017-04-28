fb00ae5f0ec6c8901fc7e4beb837a9a42874f67f
RyanMora <ryan.aimco@gmail.com>
2017-04-28 15:04:27 -0700

    finished recursion

--- intro_js_exercises/recursion.js
+++ intro_js_exercises/recursion.js
@@ -57,7 +57,7 @@ function exponent2(base, exp){
   }
 }
 console.log(exponent2(2,4));
-
+//rescursive
 function fibRec(n) {
   if (n === 0) {
     return [];
@@ -73,3 +73,126 @@ function fibRec(n) {
   }
 }
 console.log(fibRec(5));
+//iterative
+function fibIter(n){
+  if( n === 0 ){
+    return [];
+  }else if( n === 1 ){
+    return [0];
+  }else if( n === 2){
+    return [0,1];
+  }
+  let fibs = [0,1]
+  while(fibs.length != n){
+    fibs.push(fibs[fibs.length-1]+fibs[fibs.length-2])
+  }
+  return fibs;
+}
+console.log(fibIter(5));
+function bsearch(numbers, target) {
+  if (numbers.length === 0) {
+    return -1;
+  }
+
+  const probeIdx = Math.floor(numbers.length / 2);
+  const probe = numbers[probeIdx];
+  if (target === probe) {
+    return probeIdx;
+  } else if (target < probe) {
+    const left = numbers.slice(0, probeIdx);
+    return bsearch(left, target);
+  } else {
+    const right = numbers.slice(probeIdx + 1);
+    const subproblem = bsearch(right, target);
+
+    return subproblem === -1 ? -1 : subproblem + (probeIdx + 1);
+  }
+}
+console.log(bsearch([1,2,3,4,5,6,7,8],5));
+
+function makeChange(target, coins) {
+  if (target === 0) {
+    return [];
+  }
+
+  if (coins.every(el => el > target)) {
+    return null;
+  }
+
+  let bestChange = null;
+
+  function reverseSorter(a, b) {
+    if (a < b) {
+      return 1;
+    } else if (a > b) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+
+  coins.sort(reverseSorter).forEach((coin, index) => {
+    if (coin > target) {
+      return;
+    }
+
+    let remainder = target - coin;
+    // remember the optimization where we don't try to use high coins
+    // if we're already using a low one?
+    let restChange = makeChange(remainder, coins.slice(index));
+
+    if (!restChange) {
+      return;
+    }
+
+    let change = [coin].concat(restChange);
+    if (!bestChange || (change.length < bestChange.length)) {
+      bestChange = change;
+    }
+  });
+
+  return bestChange;
+}
+
+// merge, mergeSort
+function merge(left, right) {
+  const merged = [];
+
+  while (left.length > 0 && right.length > 0) {
+    let nextItem = (left[0] < right[0]) ? left.shift() : right.shift();
+    merged.push(nextItem);
+  }
+
+  return merged.concat(left, right);
+}
+
+function mergeSort(array) {
+  if (array.length < 2) {
+    return array;
+  } else {
+    const middle = Math.floor(array.length / 2);
+
+    const left = mergeSort(array.slice(0, middle));
+    const right = mergeSort(array.slice(middle));
+
+    return merge(left, right);
+  }
+}
+
+console.log(mergeSort([4, 5, 2, 3, 1]));
+
+// subsets
+function subsets(array) {
+  if (array.length === 0) {
+    return [[]];
+  }
+
+  const firstElement = array[0];
+  const subSubsets = subsets(array.slice(1));
+
+  const newSubsets = subSubsets.map(subSubset => [firstElement].concat(subSubset) );
+
+  return subSubsets.concat(newSubsets);
+}
+
+console.log(subsets([1, 3, 5]));
