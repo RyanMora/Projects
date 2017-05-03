ef997d1f4376724c50951084e33f6d7278378278
RyanMora <ryan.aimco@gmail.com>
2017-05-03 08:49:17 -0700

    finished board.js

--- jSTicTacToe/board.js
+++ jSTicTacToe/board.js
@@ -3,6 +3,90 @@ class Board{
     this.grid = Board.makeGrid();
   }
 
+  placeMark(){
+    if (!this.isEmptyPos(pos)){
+      throw new MoveError('Is not an empty position!');
+    }
+
+    this.grid[pos[0],pos[1]] = mark;
+  }
+
+  isOver(){
+    if (this.winner() != null){
+      return true;
+    }
+    for(let rowIdx = 0; rowIdx < 3; rowIdx++){
+      for(let colIdx = 0; colIdx < 3; colIdx++){
+        if(this.isEmptyPos([rowIdx, colIdx])){
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  print(){
+    const strs = [];
+    for (let rowIdx = 0; rowIdx < 3; rowIdx++){
+      const marks = [];
+      for(let colIdx = 0; colIdx < 3; colIdx++){
+        marks.push(this.grid[rowIdx][colIdx] ? this.grid[rowIdx][colIdx] : " ");
+      }
+      strs.push(`${marks.join('|')}\n`);
+    }
+    console.log(strs.join('-----\n'));
+  }
+
+  winner(){
+    const posSeqs = [
+      //horizontals
+      [[0,0],[0,1],[0,2]],
+      [[1,0],[1,1],[1,2]],
+      [[2,0],[2,1],[2,2]],
+      //verticals
+      [[0,0],[1,0],[2,0]],
+      [[0,1],[1,1],[2,1]],
+      [[0,2],[1,2],[2,2]],
+      //diagonals
+      [[0,0],[1,1],[2,2]],
+      [[2,0],[1,1],[0,2]]
+    ];
+
+    for(let i = 0; i < posSeqs.length; i++){
+      const winner = this.winnerHelper(posSeqs[i]);
+      if(winner != null){
+        return winner;
+      }
+    }
+    return null;
+  }
+
+  winnerHelper(posSeq){
+    for (let markIdx = 0; markIdx < Board.marks.length; markIdx++){
+      const targetMark = Board.marks[markIdx];
+      let winner = true;
+      for(let posIdx = 0; posIdx < 3; posIdx++){
+        const pos = posSeq[posIdx];
+        const mark = this.grid[pos[0]][pos[1]];
+
+        if (mark != targetMark){
+          winner = false;
+        }
+      }
+      if (winner){
+        return targetMark;
+      }
+    }
+    return null;
+  }
+
+  isEmptyPos(pos){
+    if(!Board.isValidPos(pos)){
+      throw new MoveError('Is not valid position!');
+    }
+    return (this.grid[pos[0]][pos[1]] === null);
+  }
+
   static isValidPos(pos){
     return (0 <= pos[0]) && (pos[0] < 3) && (0 <= pos[1]) && (pos[1] < 3)
   }
