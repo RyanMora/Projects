89ace782a22290133d022a26c906df0e2c87eef8
RyanMora <ryan.aimco@gmail.com>
2017-05-17 11:21:02 -0700

    finished phase 1

---
+++ ReactMinesweeper/components/game.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Board from './board';
+import * as Minesweeper from '../minesweeper';
+
+class Game extends React.Component {
+  constructor(props){
+    super(props);
+    const board = new Minesweeper.Board(9, 10);
+    this.state = {board: board};
+    this.updateGame = this.updateGame.bind(this);
+  }
+
+  updateGame() {
+
+  }
+
+  render(){
+    return(
+      <div>
+        <Board board={this.state.board} updateGame={this.updateGame} />
+      </div>
+    )
+  }
+}
