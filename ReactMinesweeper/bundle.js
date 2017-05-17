e882ad088794e75c59ffe145d5b737e4183095e9
RyanMora <ryan.aimco@gmail.com>
2017-05-17 11:35:31 -0700

    finished game logic

--- ReactMinesweeper/bundle.js
+++ ReactMinesweeper/bundle.js
@@ -22042,6 +22042,10 @@ module.exports = traverseAllChildren;
 "use strict";
 
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = __webpack_require__(82);
@@ -22081,14 +22085,52 @@ var Game = function (_React$Component) {
   }
 
   _createClass(Game, [{
+    key: 'restartGame',
+    value: function restartGame() {
+      var board = new Minesweeper.Board(9, 10);
+      this.setState({ board: board });
+    }
+  }, {
     key: 'updateGame',
-    value: function updateGame() {}
+    value: function updateGame(tile, flagged) {
+      if (flagged) {
+        tile.toggleFlag();
+      } else {
+        tile.explore();
+      }
+
+      this.setState({ board: this.state.board });
+    }
   }, {
     key: 'render',
     value: function render() {
+      var modal = void 0;
+      if (this.state.board.lost() || this.state.board.won()) {
+        var text = this.state.board.won() ? "You won!" : "You lost!";
+        modal = _react2.default.createElement(
+          'div',
+          { className: 'modal-screen' },
+          _react2.default.createElement(
+            'div',
+            { className: 'modal-content' },
+            _react2.default.createElement(
+              'p',
+              null,
+              text
+            ),
+            _react2.default.createElement(
+              'button',
+              { onClick: this.restartGame },
+              'Play Again'
+            )
+          )
+        );
+      }
+
       return _react2.default.createElement(
         'div',
         null,
+        modal,
         _react2.default.createElement(_board2.default, { board: this.state.board, updateGame: this.updateGame })
       );
     }
@@ -22096,6 +22138,8 @@ var Game = function (_React$Component) {
 
   return Game;
 }(_react2.default.Component);
+
+exports.default = Game;
 
 /***/ }),
 /* 183 */
--- ReactMinesweeper/components/game.jsx
+++ ReactMinesweeper/components/game.jsx
@@ -10,15 +10,41 @@ class Game extends React.Component {
     this.updateGame = this.updateGame.bind(this);
   }
 
-  updateGame() {
+  restartGame(){
+    const board = new Minesweeper.Board(9,10);
+    this.setState({board: board});
+  }
 
+  updateGame(tile, flagged) {
+    if(flagged){
+      tile.toggleFlag();
+    }else{
+      tile.explore();
+    }
+
+    this.setState({board: this.state.board});
   }
 
   render(){
-    return(
+    let modal;
+    if(this.state.board.lost() || this.state.board.won()){
+      const text = this.state.board.won() ? "You won!" : "You lost!";
+      modal =
+        <div className='modal-screen'>
+          <div className='modal-content'>
+            <p>{text}</p>
+            <button onClick={this.restartGame}>Play Again</button>
+          </div>
+        </div>;
+    }
+
+    return (
       <div>
+        {modal}
         <Board board={this.state.board} updateGame={this.updateGame} />
       </div>
     )
   }
 }
+
+export default Game;
