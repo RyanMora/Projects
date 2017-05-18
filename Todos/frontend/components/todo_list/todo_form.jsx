18b69389c8d358715212cdf73fe76c496a5dc7d7
RyanMora <ryan.aimco@gmail.com>
2017-05-18 13:03:02 -0700

    finished phase 4 - added uniqueid

---
+++ Todos/frontend/components/todo_list/todo_form.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { uniqueId } from '../../utils/idGenerator';
+
+class TooForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      title: "",
+      body: "",
+      done: false
+    };
+  }
+
+  render(){
+    <h1>hi</h1>
+  }
+}
+
+exports default TodoForm;
