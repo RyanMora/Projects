b6c0f781e8f4ad424b15c4389ef6514311fcce3d
RyanMora <ryan.aimco@gmail.com>
2017-05-19 10:03:27 -0700

    finished phase1 for todos2

--- Todos2/db/seeds.rb
+++ Todos2/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+todo1 = Todo.create(title: 'todo1', body: 'im making a todo1', done: false)
+todo2 = Todo.create(title: 'todo2', body: 'im making a todo2', done: false)
+todo3 = Todo.create(title: 'todo3', body: 'im making a todo3', done: false)
+todo4 = Todo.create(title: 'todo4', body: 'im making a todo4', done: false)
