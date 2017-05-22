c8044339fd5f826c0f1ceaa54f55198361117018
RyanMora <ryan.aimco@gmail.com>
2017-05-22 11:26:03 -0700

    phase 0 complete for pokekedex

---
+++ Pokedex/app/controllers/api/pokemon_controller.rb
@@ -0,0 +1,11 @@
+class Api::PokemonController < ApplicationController
+  def index
+    sleep 0.5
+    @pokemon = Pokemon.all
+  end
+
+  def show
+    sleep 0.5
+    @pokemon = Pokemon.find(params[:id])
+  end
+end
---
+++ Pokedex/app/views/api/pokemon/show.json.jbuilder
@@ -0,0 +1,7 @@
+
+json.extract! @pokemon, :id, :name, :attack, :defense, :moves, :poke_type
+json.image_url asset_path(@pokemon.image_url)
+
+json.items do
+  json.array! @pokemon.items
+end
