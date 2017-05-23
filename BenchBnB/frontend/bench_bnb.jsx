f2aba75a3d1c01957c8f44f04b5a5ab4cf5dd477
RyanMora <ryan.aimco@gmail.com>
2017-05-23 12:09:02 -0700

    finished phase1 - frontend user auth

--- BenchBnB/frontend/bench_bnb.jsx
+++ BenchBnB/frontend/bench_bnb.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  window.store = configureStore();
   const root = document.getElementById('root');
   ReactDom.render(<h1>Welcome to BenchBnB</h1>, root);
 });
