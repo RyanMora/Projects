c6f1b7d685e0b810505d3db3e06b2dfeb922c746
RyanMora <ryan.aimco@gmail.com>
2017-05-23 12:59:24 -0700

    finished phase2 react router and session components

--- BenchBnB/frontend/bench_bnb.jsx
+++ BenchBnB/frontend/bench_bnb.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.store = configureStore();
+  const store = configureStore();
   const root = document.getElementById('root');
-  ReactDom.render(<h1>Welcome to BenchBnB</h1>, root);
+  ReactDom.render(<Root store={store}/>, root);
 });
---
+++ BenchBnB/frontend/components/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+import GreetingContainer from './greeting/greeting_container';
+import SessionFormContainer from './session_form/session_form_container';
+
+const App = () => (
+  <div>
+    <header>
+      <h1>Bench Bnb</h1>
+      <GreetingContainer/>
+    </header>
+    <Route path="/login" component={SessionFormContainer}/>
+    <Route path="/signup" component={SessionFormContainer}/>
+  </div>
+);
+
+export default App;
